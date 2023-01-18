47f35ae4dcce317f9af7565703caf8a66ab51c91
Alevtyna777 <119439779+Alevtyna777@users.noreply.github.com>
2023-01-18 16:45:53 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Person person = new Person();
+        System.out.println(person.personInfo ("Will" , "Smith" , "New York", "2936729462846"));
+        System.out.println(person.personInfo ("Jackie", "Chan", "Shanghai", "12312412412"));
+        System.out.println(person.personInfo ("Sherlock", " Holmes","London", "37742123513"));
     }
 }
